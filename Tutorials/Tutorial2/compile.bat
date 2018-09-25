80ce9b322b843d04fd2cf94c8ca43842f7e8e65f
Maginor84 <magnus.dahler.norling@gmail.com>
2018-09-25 16:45:36 +0200

    Added tutorial2 and 3

---
+++ Tutorials/Tutorial2/compile.bat
@@ -0,0 +1,2 @@
+@echo off
+C:\Mingw\bin\g++ tutorial2.cpp -O2 -fno-exceptions -o tutorial2.exe -fmax-errors=5
---
+++ Tutorials/Tutorial3/compile.bat
@@ -0,0 +1,2 @@
+@echo off
+C:\Mingw\bin\g++ tutorial3.cpp -O2 -fno-exceptions -o tutorial3.exe -fmax-errors=5

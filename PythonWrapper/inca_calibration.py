a9a28978980e2de0527a413eaed161872b333446
Maginor84 <magnus.dahler.norling@gmail.com>
2019-01-25 14:31:31 +0100

    Fikst bug

--- PythonWrapper/inca_calibration.py
+++ PythonWrapper/inca_calibration.py
@@ -1,5 +1,4 @@
 
-import matplotlib.pyplot as plt, seaborn as sn, emcee, corner, mpld3
 from scipy import optimize
 import numpy as np
 import numdifftools as nd
@@ -22,22 +21,22 @@ def check_min_max(params, min, max):
 def run_optimization(dataset, min, max, initial_guess, calibration, objective, minimize=True) :
 	objective_fun = objective[0]
 
-	if skiptimesteps >= dataset.get_parameter_uint('Timesteps', []) :
-		raise ValueError('We were told to skip more timesteps in the evaluation of the objective than the amount of timesteps we run the model for.')
+	#if skiptimesteps >= dataset.get_parameter_uint('Timesteps', []) :
+	#	raise ValueError('We were told to skip more timesteps in the evaluation of the objective than the amount of timesteps we run the model for.')
 	
 	if minimize :
 		def eval(params) :
 			# NOTE: This version of the Nelder-Mead algorithm does not allow for bounds, so we have to hack them in
 			if not check_min_max(params, min, max):
 				return np.inf
-			return objective_fun(params, dataset, calibration, objective, obsseries) 
+			return objective_fun(params, dataset, calibration, objective) 
 		
 		return optimize.fmin(eval, initial_guess, maxfun=10000)
 	else :
 		def eval(params) :
 			if not check_min_max(params, min, max):
 				return np.inf
-			return -objective_fun(params, dataset, calibration, objective, obsseries) 
+			return -objective_fun(params, dataset, calibration, objective) 
 		
 		return optimize.fmin(eval, initial_guess, maxfun=10000)
 
@@ -71,9 +70,36 @@ def constrain_min_max(dataset, calibration, minvec, maxvec) :
 		if minvec[idx] < min : minvec[idx] = min
 		if maxvec[idx] > max : maxvec[idx] = max
 
-		
-		
-		
+def log_likelyhood(params, dataset, calibration, objective):	
+	# NOTE: If we use a parallellized optimizer we need to make a copy of the dataset to not have several threads overwrite each other.
+	# (in that case, only use the copy when setting parameter values, running the model, and extracting results below)
+	datasetcopy = dataset.copy()
+	
+	set_values(datasetcopy, params, calibration)
+	
+	datasetcopy.run_model()
+	
+	fn, simname, simindexes, obsname, obsindexes, skiptimesteps = objective
+    
+	sim = datasetcopy.get_result_series(simname, simindexes)
+	obs = datasetcopy.get_input_series(obsname, obsindexes, alignwithresults=True)
+	
+	sim2 = sim[skiptimesteps:]
+	obs2 = obs[skiptimesteps:]
+	
+	M = params[len(calibration)]
+	sigma_e = M*sim2
+	
+	likes = norm(sim2, sigma_e).logpdf(obs2)
+	
+	like = np.nansum(likes)
+    
+	# NOTE: If we made a copy of the dataset we need to delete it so that we don't get a huge memory leak
+	datasetcopy.delete()
+	
+	#print ('single evaluation. result: %f' % like)
+	
+	return like	
 		
 		
 	

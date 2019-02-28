4ff5505d53a010fcaf2fdc6756b61c4ee9448648
Leah Jackson-Blake <leah.jackson-blake@niva.no>
2019-02-28 12:47:48 +0100

    Revert "Compiler files for PERSiST and SimplyP, and sqlite3"
    This reverts commit 268461de147dd3ac8b6867a148d6ad54c806736b.

--- Applications/Persist/compile_incaview_persist.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-g++ persist.cpp -O2 -std=c++11 -fno-exceptions -o persist.exe ../../sqlite3/sqlite3.o -fmax-errors=5
--- Applications/SimplyP/compile_incaview.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-g++ incaviewsimplyp.cpp -O2 -std=c++11 -fno-exceptions -o incaviewsimplyp.exe ../../sqlite3/sqlite3.o -fmax-errors=5
